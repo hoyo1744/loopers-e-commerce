19ec0cdbcf000fa832c4e2a41767540b667a087b
hoyong.eom <hoyo1744@gmail.com>
2025-07-18 01:10:12 +0900

    feat: 포인트 interfaces 레이어 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/point/PointV1Controller.java
@@ -0,0 +1,30 @@
+package com.loopers.interfaces.api.point;
+
+import com.loopers.application.point.AppPointResult;
+import com.loopers.application.point.PointFacade;
+import com.loopers.interfaces.api.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/points")
+public class PointV1Controller implements PointV1ApiSpec {
+
+    private final PointFacade pointFacade;
+
+
+    @Override
+    @GetMapping
+    public ApiResponse<PointResponse.Point> getPoints(@RequestHeader(value = "X-USER-ID") String userId) {
+        AppPointResult.Point result = pointFacade.getPoint(userId);
+        return ApiResponse.success(PointResponse.Point.of(result.getPoint()));
+    }
+
+    @Override
+    @PostMapping("/charge")
+    public ApiResponse<PointResponse.ChargedPoint> charge(@RequestHeader("X-USER-ID") String userId, @RequestBody Long amount) {
+        AppPointResult.ChargedPoint charge = pointFacade.charge(userId, amount);
+        return ApiResponse.success(PointResponse.ChargedPoint.of(charge.getPoint()));
+    }
+}
