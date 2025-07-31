7b0ee4bdd02f487514752ad6d4bec816a408e112
hoyong.eom <hoyo1744@gmail.com>
2025-08-01 01:58:05 +0900

    feat: Stock 도메인 및 기능 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/stock/StockRepository.java
@@ -0,0 +1,8 @@
+package com.loopers.domain.stock;
+
+public interface StockRepository {
+
+    Stock findByProductId(Long productId);
+
+    Stock save(Stock stock);
+}
---
+++ apps/commerce-api/src/main/java/com/loopers/domain/stock/StockService.java
@@ -0,0 +1,43 @@
+package com.loopers.domain.stock;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    public boolean isStockAvailable(Long productId, Long quantity) {
+        Stock stock = stockRepository.findByProductId(productId);
+        return stock.hasEnough(quantity);
+    }
+
+    public StockInfo.Stock getStock(Long produdctId) {
+        Stock stock = stockRepository.findByProductId(produdctId);
+        return StockInfo.Stock.of(stock.getProductId(), stock.getQuantity());
+    }
+
+    @Transactional
+    public void increaseStock(StockCommand.OrderProducts orderProducts) {
+        orderProducts.getOrderProducts()
+                .forEach(orderProduct -> {
+                    Stock stock = stockRepository.findByProductId(orderProduct.getProductId());
+                    stock.incrementQuantity(orderProduct.getQuantity());
+                });
+    }
+
+    @Transactional
+    public void decreaseStock(StockCommand.OrderProducts orderProducts) {
+        orderProducts.getOrderProducts()
+                .forEach(orderProduct -> {
+                    Stock stock = stockRepository.findByProductId(orderProduct.getProductId());
+                    stock.decrementQuantity(orderProduct.getQuantity());
+                });
+    }
+
+}
