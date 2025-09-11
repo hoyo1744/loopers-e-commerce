1e9de98b6a22e1f4a0c4cfb882b14ec30a53e20d
hoyong.eom <hoyo1744@gmail.com>
2025-09-11 22:49:09 +0900

    feat: commerce-api, 주문 이벤트 발행

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/event/external/ExternalDataPlatformListener.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/event/external/ExternalDataPlatformListener.java
@@ -1,12 +1,19 @@
 package com.loopers.interfaces.api.event.external;
 
+import com.loopers.common.kafka.event.EventMessage;
+import com.loopers.common.kafka.event.EventType;
+import com.loopers.common.kafka.event.order.OrderOutEvent;
+import com.loopers.common.kafka.topic.Topics;
 import com.loopers.domain.external.ExternalDataService;
 import com.loopers.domain.external.OrderResultPayload;
 import com.loopers.domain.order.OrderEvent;
+import com.loopers.domain.sender.MessageSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
@@ -14,9 +21,30 @@ public class ExternalDataPlatformListener {
 
     private final ExternalDataService externalDataService;
 
+    private final MessageSender messageSender;
+
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void handle(OrderEvent.Completed event) {
         externalDataService.send("ORDER", OrderResultPayload.of(event.getOrderId(), event.getOrderNumber(), event.getAmount()));
+
+        event.getOrderProducts().stream().forEach(
+                orderProduct -> {
+                    messageSender.send(Topics.ORDER,
+                            event.getUserId().toString(),
+                            EventMessage.<OrderOutEvent.Order>builder()
+                                    .eventType(EventType.ORDER_COMPLETED)
+                                    .version("v1")
+                                    .payload(OrderOutEvent.Order.of(
+                                            event.getUserId(),
+                                            orderProduct.getProductId(),
+                                            event.getOrderId(),
+                                            event.getOrderNumber(),
+                                            orderProduct.getQuantity(),
+                                            LocalDateTime.now()))
+                                    .build()
+                    );
+                }
+        );
     }
 
 }
