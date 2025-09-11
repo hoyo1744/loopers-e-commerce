ba47e498282336812d0f3d58026616a7dd9bcc6f
hoyong.eom <hoyo1744@gmail.com>
2025-09-11 22:49:31 +0900

    feat: commerce-api, 주문 이벤트 테스트(이벤트 발행 mock 추가)

--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/event/external/ExternalDataPlatformListenerTest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/event/external/ExternalDataPlatformListenerTest.java
@@ -3,10 +3,13 @@ package com.loopers.interfaces.api.event.external;
 import com.loopers.domain.external.ExternalDataService;
 import com.loopers.domain.external.OrderResultPayload;
 import com.loopers.domain.order.OrderEvent;
+import com.loopers.domain.sender.MessageSender;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -15,7 +18,8 @@ import static org.mockito.Mockito.verify;
 class ExternalDataPlatformListenerTest {
 
     private final ExternalDataService externalDataService = mock(ExternalDataService.class);
-    private final ExternalDataPlatformListener listener = new ExternalDataPlatformListener(externalDataService);
+    private final MessageSender messageSender = mock(MessageSender.class);
+    private final ExternalDataPlatformListener listener = new ExternalDataPlatformListener(externalDataService, messageSender);
 
     @Nested
     @DisplayName("주문 완료 외부 데이터 연동 이벤트 테스트")
@@ -25,10 +29,11 @@ class ExternalDataPlatformListenerTest {
         @DisplayName("Order 완료 이벤트를 수신하면 외부 전송 함수가 호출된다.")
         void callSendToExternalPlatform_whenOrderCompletedEventReceived() {
             // given
+            String userId = "hoyong";
             Long orderId = 1L;
             String orderNumber = "ORDER-123456";
             Long amount = 15000L;
-            OrderEvent.Completed event = OrderEvent.Completed.of(orderId, orderNumber, amount);
+            OrderEvent.Completed event = OrderEvent.Completed.of(userId, orderId, orderNumber, amount, List.of(OrderEvent.OrderProduct.of(1L, 1L)));
 
             // when
             listener.handle(event);
