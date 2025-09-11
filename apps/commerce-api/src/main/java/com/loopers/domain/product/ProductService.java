4c1ccb6f923c01c7c599518f6ba6d0544af358a6
hoyong.eom <hoyo1744@gmail.com>
2025-09-11 22:36:12 +0900

    feat: commerce-api, 제품 상세 조회시 랭킹 정보 추가

--- apps/commerce-api/src/main/java/com/loopers/domain/product/ProductService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/ProductService.java
@@ -1,9 +1,15 @@
 package com.loopers.domain.product;
 
+import com.loopers.common.kafka.event.EventMessage;
+import com.loopers.common.kafka.event.EventType;
+import com.loopers.common.kafka.event.pageview.PageViewOutEvent;
+import com.loopers.common.kafka.topic.Topics;
+import com.loopers.domain.sender.MessageSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -12,11 +18,28 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    private final MessageSender messageSender;
+
     public List<ProductInfo.ProductQuery> getProducts(ProductCommand.Search command) {
         return productRepository.search(command);
     }
 
     public ProductInfo.ProductDetail getProductDetail(Long productId) {
+        Product product = productRepository.findById(productId);
+        return ProductInfo.ProductDetail.of(product.getName(), product.getPrice(), product.getBrandId());
+    }
+
+    public ProductInfo.ProductDetail getProduct(Long productId) {
+        messageSender.send(Topics.PAGE_VIEW, productId.toString(),
+                EventMessage.<PageViewOutEvent.Viewed>builder()
+                        .eventType(EventType.PAGE_VIEWED)
+                        .version("v1")
+                        .payload(PageViewOutEvent.Viewed.of(
+                                productId,
+                                LocalDateTime.now()))
+                        .build()
+        );
+
         Product product = productRepository.findById(productId);
         return ProductInfo.ProductDetail.of(product.getName(), product.getPrice(), product.getBrandId());
     }
